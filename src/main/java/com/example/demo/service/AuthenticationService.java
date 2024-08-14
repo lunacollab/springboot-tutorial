f14927fee546bfc043b654c868f9ecbf57af4d0b
lunacollab <justmind234@gmail.com>
2024-08-15 06:56:16 +0700

    spring-tutorial

--- src/main/java/com/example/demo/service/AuthenticationService.java
+++ src/main/java/com/example/demo/service/AuthenticationService.java
@@ -82,7 +82,7 @@ public class AuthenticationService {
 
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
                 .subject(user.getUsername())
-                .issuer("devteria.com")
+                .issuer("demo")
                 .issueTime(new Date())
                 .expirationTime(new Date(
                         Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
